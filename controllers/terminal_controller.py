c1fafc9ef04b0dbaf69ba78732339fcc0dcc8bc0
Erik <94122538+eryk-2612@users.noreply.github.com>
2026-05-09 15:19:21 +0200

    Add files via upload

--- controllers/terminal_controller.py
+++ controllers/terminal_controller.py
@@ -1,3 +1,4 @@
+from states import MessageboxState
 from core import Events, Logo, TokensDE, Focus, Screens, Colors, Others
 import curses
 
@@ -14,14 +15,17 @@ class TerminalController:
         else:
             return None
 
-    def get_focus_window(self):
-        return self.view.get_window(self.state.focus)
+    def get_window(self, fullscreen=False):
+        if fullscreen:
+            return self.view.get_window()
+        else:
+            return self.view.get_window(self.state)
 
     def init_state(self):
         self.state.running = True
         self.state.screen = Screens.SIGNIN
-        self.state.event = Events.SIGNIN
-        self.state.focus = Focus.SIGNIN
+        self.state.event = None
+        self.state.focus = None
 
     def run(self):
         self.init_state()
@@ -30,88 +34,90 @@ class TerminalController:
             self.update_state()
             self.draw_view()
 
-    def update_state(self):
-        pass
+            self.state.msgbox = None
+            self.state.event = None
 
-    def continue_signin(self):
-        self.state.event = Events.CONTINUE
-        self.state.focus = Focus.BOOT
+    def handle_input(self):
+        window = self.get_window()
+        key = self.get_input(window)
+        if key in [10, 13]:
+            self.enter_pressed()
+        elif key in range(ord('0'), ord('9') + 1):
+            self.number_pressed(key)
+        elif key in [curses.KEY_BACKSPACE, 127, 8]:
+            self.backspace_pressed()
+
+    def enter_pressed(self):
+        screen = self.state.screen
+        focus = self.state.focus
+        entered_code = self.state.entered_code
+        if screen == Screens.SIGNIN:
+            self.continue_boot()
+        if screen == Screens.BOOT and focus == Focus.LOCK:
+            if len(entered_code) == len(self.model.unlock_code):
+                self.unlock_terminal(entered_code)
+
+    def number_pressed(self, key):
+        focus = self.state.focus
+        entered_code = self.state.entered_code
+        if focus == Focus.LOCK:
+            self.enter_code(entered_code, key)
+
+    def backspace_pressed(self):
+        focus = self.state.focus
+        entered_code = self.state.entered_code
+        if focus == Focus.LOCK:
+            self.undo_enter_code(entered_code)
+
+    def continue_boot(self):
         self.state.screen = Screens.BOOT
 
     def unlock_terminal(self, entered_code):
         if entered_code == self.model.unlock_code:
-            self.state.event = Events.TERM_LOCK_SUC
+            self.state.msgbox = MessageboxState(True, TokensDE.MSG_SUCCESS, Colors.SELECTED)
+            self.model.unlock()
+            self.state.entered_code = ""
+            self.state.focus = None
         else:
-            self.state.event = Events.TERM_LOCK_FAIL
+            self.state.msgbox = MessageboxState(True, TokensDE.MSG_FAIL, Colors.WARNING)
+            self.state.entered_code = ""
         self.state.focus_window = self.view.get_window()
-        self.state.focus = Focus.BOOT
 
     def enter_code(self, entered_code, key):
         if len(entered_code) < len(self.model.unlock_code):
             self.state.entered_code += chr(key)
-            self.state.event = Events.LOCK_TYPE
 
     def undo_enter_code(self, entered_code):
         if entered_code:
             self.state.entered_code = self.state.entered_code[:-1]
-            self.state.event = Events.LOCK_TYPE
 
-    def handle_input(self):
-        focus_window = self.get_focus_window()
-        focus = self.state.focus
+    def update_state(self):
         screen = self.state.screen
+        event = self.state.event
+        focus = self.state.focus
         entered_code = self.state.entered_code
 
-        key = self.get_input(focus_window)
-        if key in [10, 13]:
-                if screen == Screens.SIGNIN:
-                    self.continue_signin()
-                if screen == Screens.BOOT and focus == Focus.LOCK:
-                    if len(entered_code) == len(self.model.unlock_code):
-                        self.unlock_terminal(entered_code)
-        elif key in range(ord('0'), ord('9') + 1):
-            if screen == Screens.BOOT and focus == Focus.LOCK:
-                self.enter_code(entered_code, key)
-        elif key in [curses.KEY_BACKSPACE, 127, 8]:
-            if screen == Screens.BOOT and focus == Focus.LOCK:
-                self.undo_enter_code(entered_code)
+        if screen == Screens.BOOT:
+            if self.model.locked:
+                self.state.focus = Focus.LOCK
 
     def draw_view(self):
-        match self.state.event:
-            case Events.SIGNIN:
+        screen = self.state.screen
+        focus = self.state.focus
+        event = self.state.event
+        msgbox = self.state.msgbox or MessageboxState(False, "", 0)
+        match screen:
+            case Screens.SIGNIN:
                 self.view.draw_signin(parent_window=self.view.get_window(), image=TokensDE.SIGNIN)
-                self.state.event = None
-            case Events.CONTINUE:
-                self.view.draw_signin(parent_window=self.view.get_window(), image=TokensDE.SIGNIN, undraw=True)
-                self.state.event = Events.LOCK_CHECK
-            case Events.LOCK_CHECK:
-                if self.model.locked:
-                    self.state.event = Events.TERM_LOCKED
-                elif not self.model.locked:
-                    self.state.event = Events.BOOT
-                    self.state.focus = Focus.BOOT
-                    self.state.screen = Screens.BOOT
-            case Events.BOOT:
+            case Screens.BOOT:
+                self.view.undraw_signin(parent_window=self.view.get_window(), image=TokensDE.SIGNIN)
                 self.view.draw_footer(Others.COPYRIGHT)
-                self.view.draw_startup_animation(self.view.get_window(), Logo.DEFAULT)
-                self.state.event = None
-            case Events.TERM_LOCKED:
-                self.view.draw_footer(Others.COPYRIGHT)
-                self.view.draw_lock(self.model.unlock_code, self.view.get_window())
-                self.state.focus = Focus.LOCK
-                self.state.event = None
-            case Events.LOCK_TYPE:
-                self.get_focus_window().type(self.state.entered_code)
-                self.state.event = None
-            case Events.TERM_LOCK_SUC:
-                self.view.draw_messagebox(TokensDE.MSG_SUCCESS.upper(), Colors.SELECTED, self.get_focus_window())
-                self.model.unlock()
-                self.state.entered_code = ""
-                self.state.event = Events.LOCK_CHECK
-            case Events.TERM_LOCK_FAIL:
-                self.view.draw_messagebox(TokensDE.MSG_FAIL.upper(), Colors.WARNING, self.get_focus_window())
-                self.state.entered_code = ""
-                self.state.event = Events.LOCK_CHECK
-
-
-
+                if focus == Focus.LOCK:
+                    self.view.draw_lock(self.model.unlock_code, self.view.get_window())
+                    self.get_window().type(self.state.entered_code)
+                    if msgbox.show:
+                        self.view.draw_messagebox(msgbox.message.upper(), msgbox.color, self.get_window(True))
+                else:
+                    if msgbox.show:
+                        self.view.draw_messagebox(msgbox.message.upper(), msgbox.color, self.get_window(True))
+                    self.view.draw_startup_animation(self.view.get_window(), Logo.DEFAULT)
